37e5c60ebd896a90d9e5270aeee6b4ac689bd6c1
Lara Sanders <larasanders@gmail.com>
2017-11-27 20:47:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,5 +30,11 @@ def save
   INSERT INTO students(nane, grade) VALUES (?,?)
   SQL
   DB[:conn].execute(sql, self.name, self.table)
-  
+end
+
+def self.create(name:, grade:)
+  student = Student.new(name, grade)
+  student.save
+  student
+end
 end
